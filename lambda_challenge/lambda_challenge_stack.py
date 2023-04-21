af72d4910e40cfc4f1aa5e7c2123d9b9acde6443
Max T <hustenbonbon@posteo.de>
2023-04-21 16:18:04 +0200

    Add a managing and a worker function and SQS

--- lambda_challenge/lambda_challenge_stack.py
+++ lambda_challenge/lambda_challenge_stack.py
@@ -1,19 +1,56 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    Tags,
+    aws_lambda,
+    aws_apigateway,
+    aws_sqs,
+    aws_lambda_event_sources,
+    aws_lambda_nodejs,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
-class LambdaChallengeStack(Stack):
 
+class LambdaChallengeStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        queue = aws_sqs.Queue(self, "worker-queue")
+        handler = aws_lambda_nodejs.NodejsFunction(
+            self,
+            "manager",
+            entry="entrypoint.ts",
+            handler="lambdaHandler",
+            deps_lock_file_path="yarn.lock",
+            runtime=aws_lambda.Runtime.NODEJS_18_X,
+            environment=dict(WORKER_QUEUE=queue.queue_url),
+        )
+        queue.grant_send_messages(handler)
 
-        # The code that defines your stack goes here
+        workers = aws_lambda.Function(
+            self,
+            "workerX",
+            runtime=aws_lambda.Runtime.NODEJS_18_X,
+            code=aws_lambda.Code.from_asset("workers"),
+            handler="example.main",
+        )
+        queue.grant_consume_messages(workers)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LambdaChallengeQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        workers.add_event_source(
+            aws_lambda_event_sources.SqsEventSource(
+                queue, max_concurrency=2, batch_size=1
+            )
+        )
+
+        api = aws_apigateway.RestApi(
+            self,
+            "telephone-game-api",
+            rest_api_name="Telephone game",
+            description="This api handles strings like the children game telephone",
+        )
+
+        entrypoint_integration = aws_apigateway.LambdaIntegration(
+            handler, request_templates={"application/json": '{ "statusCode": "200" }'}
+        )
+
+        api.root.add_method("GET", entrypoint_integration)
